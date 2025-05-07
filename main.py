5ea58c5a1a3e24f3a4c708803940a4bd66bbb138
LeeSH <swhio121@g.skku.du>
2025-05-07 18:54:39 +0900

    pass error solving 1

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
-    """Reads a file and returns a list of lines in the file"""
-    # Will this be working?
     lines = open(path, 'r').read().split('\n')
     return lines
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
-    """Converts two lists of file paths into a list of json strings"""
-    # Preprocess unwanted characters
     def process_file(file):
         if '\\' in file:
             file = file.replace('\\', '\\\\')
@@ -22,7 +18,6 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     template_mid = '\",\"German\":\"'
     template_end = '\"}'
 
-    # Can this be working?
     processed_file_list = []
     for english_file, german_file in zip(english_file_list, german_file_list):
         english_file = process_file(english_file)
@@ -32,7 +27,6 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     return processed_file_list
 
 def write_file_list(file_list: List[str], path: str) -> None:
-    """Writes a list of strings to a file, each string on a new line"""
     with open(path, 'w') as f:
         for file in file_list:
             f.write(file + '\n')
